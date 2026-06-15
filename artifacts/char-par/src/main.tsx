77e2e5425b738131a26d7a2443cfe22214d3f188
AbdulNabi9966 <122974594+AbdulNabi9966@users.noreply.github.com>
2026-06-15 18:30:01 +0530

    Update main.tsx

--- artifacts/char-par/src/main.tsx
+++ artifacts/char-par/src/main.tsx
@@ -1,10 +1,18 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { setAuthTokenGetter } from "@workspace/api-client-react";
+// import { setAuthTokenGetter } from "@workspace/api-client-react";
+import {
+  setAuthTokenGetter,
+  setBaseUrl
+} from "@workspace/api-client-react";
+
 import { useAuthStore } from "./store/authStore";
 
 // Wire up auth token for all API calls
+
+setBaseUrl(import.meta.env.VITE_API_URL);
+
 setAuthTokenGetter(() => useAuthStore.getState().token);
 
 createRoot(document.getElementById("root")!).render(<App />);
